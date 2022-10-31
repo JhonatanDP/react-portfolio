d8c1bd45efa58f1a1c372b0c7952c3d324b3a4ff
Jhonatan <jonathandiazp@gmail.com>
2022-10-31 16:14:50 -0400

    contact index

---
+++ src/components/Contact/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
+function ContactForm() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const { name, email, message } = formState;
+
+    
