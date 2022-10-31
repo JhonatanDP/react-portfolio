c114259571d2b6f89cb5eacf59a8c9c0cecf78d1
Jhonatan <jonathandiazp@gmail.com>
2022-10-31 14:22:14 -0400

    nav part1

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,3 +2,19 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 
+function Navigation(props) {
+    return (
+      <div>
+        <Navbar expand="lg" bg="dark" sticky="top">
+          <NavLink className="nav-link" to="/">
+            <div class="text-light">
+              <h4 class="nav-title-font">Jhonatan Diaz</h4>
+            </div>
+          </NavLink>
+         
+        </Navbar>
+      </div>
+    );
+  }
+  
+  export default Navigation;
