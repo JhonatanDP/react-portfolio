1c0d864b283b0d0da2dcce4455f5c81a90e21d7c
Jhonatan <jonathandiazp@gmail.com>
2022-10-31 14:08:46 -0400

    about

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section className="container">
+            <h2 class="top-title">Jhonatan Diaz</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/106892660?s=400&u=6269a616229a9bdcd1b80feee0ae889c3da78df9&v=4" alt="Jhonatan Diaz"/>
+
+               
+            </div>
+        </section>
+    )
+}
+
+export default About;
