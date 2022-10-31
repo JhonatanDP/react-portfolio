6b319eef3d24f30b10d5cb20087cde9a3be85a51
Jhonatan <jonathandiazp@gmail.com>
2022-10-31 17:10:28 -0400

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <footer className="footer bg-dark fixed-bottom">
+        <a href="https://https://github.com/JhonatanDP"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+    </footer>
+    )
+}
+
+export default Footer;
