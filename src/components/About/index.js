796d0dc2829e0f3888f58f72ef1b0ba3e465db13
Jhonatan <jonathandiazp@gmail.com>
2022-10-31 14:17:06 -0400

    about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,14 @@ function About() {
             <hr></hr>
             <div>
                 <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/106892660?s=400&u=6269a616229a9bdcd1b80feee0ae889c3da78df9&v=4" alt="Jhonatan Diaz"/>
-
+                <p>
+                Accomplished system administrator with 18 years of experience managing server infrastructure and data-center operations across
+                Multiple platforms (Unix, Linux, Windows). Effectively plan, install, configure and optimize the IT infrastructure to consistently 
+                achieve high availability and performance.
+                </p>
+                <p>
+                Great knowledge about systems, networks and hardware.
+                </p>
                
             </div>
         </section>
