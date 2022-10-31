15a73809593ccb23108065e252a1fec68e55859b
Jhonatan <jonathandiazp@gmail.com>
2022-10-31 16:07:30 -0400

    header added component

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Route, Redirect, HashRouter } from "react-router-dom";
+import Navigation from "../../components/Navigation";
+import About from "../../components/About";
+import Portfolio from "../../components/Portfolio";
+
+
+
+class Header extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <header>
+          <Navigation />
+        </header>
+
+        <div className="content">
+          <Route exact path="/" render={() => <Redirect to="/about" />} />
+          <Route path="/about" component={About} />
+          <Route path="/portfolio" component={Portfolio} />
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default Header;
